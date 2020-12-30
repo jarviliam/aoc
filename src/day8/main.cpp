bb07597cc7f23fb98b1897a2095624965d35ebd8
jarviliam <jarviliam@gmail.com>
2020-12-30 21:18:07 +0900

    day8 - c++17

--- src/day8/main.cpp
+++ src/day8/main.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <iostream>
+#include <set>
+#include <tuple>
 #include <unordered_set>
 #include <vector>
 
@@ -12,21 +14,28 @@ void loadIt(std::vector<std::string> &input) {
     };
   };
 };
+
+std::tuple<std::string, int> parse(const std::string x) {
+  int space = x.find(" ");
+  std::string inst = x.substr(0, space);
+  int val = std::stoi(x.substr(space + 1, x.length()));
+  return {inst, val};
+};
+
 int main() {
   std::vector<std::string> instructions;
   loadIt(instructions);
 
   int accum = 0;
   std::unordered_set<int> jmpList = {};
+  std::vector<int> instList = {};
 
   std::vector<std::string>::iterator itr;
 
   for (itr = instructions.begin(); itr <= instructions.end(); itr++) {
 
-    int space = (*itr).find(" ");
-    std::string inst = (*itr).substr(0, space);
-    int val = std::stoi((*itr).substr(space + 1, (*itr).length()));
-    std::cout << inst << std::endl;
+    // C++17 Thank you.
+    auto [inst, val] = parse((*itr));
 
     if (jmpList.find(itr - instructions.begin()) != jmpList.end()) {
       // This instruction has already be ran
@@ -38,10 +47,27 @@ int main() {
     } else if (inst == "jmp") {
       std::cout << "Jumping " << accum << " " << val << std::endl;
       // Emplace Before Advancing Itr
+      instList.emplace_back(itr - instructions.begin());
       jmpList.emplace(itr - instructions.begin());
       std::advance(itr, val - 1);
       continue;
+    } else {
+      if ((instructions.end() - itr) == val - 1) {
+        std::cout << "Should Jump from Nop " << std::endl;
+      }
     }
     jmpList.emplace(itr - instructions.begin());
+    instList.emplace_back(itr - instructions.begin());
   }
+
+  std::vector<int>::reverse_iterator rIt;
+  // TODO(jarviliam): Reverse Loop -> If Jump -> Attempt to finish by changing
+  // to Nop / Vice Versa
+  for (rIt = instList.rbegin(); rIt != instList.rend(); rIt++) {
+    auto [inst, val] = parse(instructions[(*rIt)]);
+    std::cout << instructions[*rIt] << std::endl;
+    if (inst == "jmp") {
+      std::cout << "Reverse Jump " << (*rIt) << std::endl;
+    };
+  };
 };
