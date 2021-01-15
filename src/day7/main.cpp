cf4aa096bf9ef8496d4dfc6520c03ee501aa617e
jarviliam <jarviliam@gmail.com>
2021-01-16 00:36:26 +0900

    day7 c++ pt1

--- src/day7/main.cpp
+++ src/day7/main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <deque>
 #include <fstream>
 #include <iostream>
 #include <unordered_map>
@@ -15,47 +16,78 @@ void loadIt(std::vector<std::string> &input) {
   };
 };
 
-struct Node {
-  bool counted;
-  std::string type;
-  std::vector<Node> children;
-  Node(std::string type, bool counted) : type(type),counted(counted) {}
-};
+std::vector<std::string> split(std::string &s, std::string delimiter) {
+  std::vector<std::string> list;
+  size_t pos = 0;
+  std::string token;
+  while ((pos = s.find(delimiter)) != std::string::npos) {
+    token = s.substr(0, pos);
+    list.push_back(token);
+    s.erase(0, pos + delimiter.length());
+  }
+  list.push_back(s);
+  return list;
+}
 
-// Type -> List Children
-// X contain Y z bags, etc...
-// Check no other bags
-//
-// Split at contain , number , bag
-int main() {
-  std::vector<std::string> instructions;
-  std::unordered_map<std::string, Node> nodeList;
-  loadIt(instructions);
+int part1(std::vector<std::string> l) {
+  int ans;
 
-  for (auto &i : instructions) {
-    auto itr = i.find("contain");
-    std::string key = i.substr(0, itr);
-    i.erase(0, itr + 8);
-    auto no = i.find("no");
-    std::cout << i << std::endl;
-    if (no != std::string::npos) {
-      Node test = Node(key,false);
-      nodeList.emplace(key, test);
-    } else {
-        while(i.find(",") == std::string::npos || i.find(".") == std::string::npos){
-            int amount = std::stoi(i.substr(0,1));   
-            i.erase(0,2);
-            auto delim = i.find("bag");
-            std::string type = i.substr(0,delim-1);
-            i.erase(0,delim);
-            auto commaDelim = i.find(",");
-            if(amount > 1){
-                //i.erase(0,
-            }
+  std::unordered_map<std::string, std::vector<std::string>> list;
+  for (auto i : l) {
+    if (i != "") {
+      auto words = split(i, " ");
+      std::string holder = words[0] + words[1] + words[2];
+      holder.erase(holder.size() - 1, 1);
+      if (words[words.size() - 3] == "no") {
+        continue;
+      };
+      int idx = 4;
+      while (idx < words.size()) {
+        std::string bag =
+            words[idx] + words[idx + 1] + words[idx + 2] + words[idx + 3];
+        // Clean bag
+        if (bag[bag.size() - 1] == '.') {
+          bag.erase(bag.size() - 1, 1);
+        }
+        if (bag[bag.size() - 1] == ',') {
+          bag.erase(bag.size() - 1, 1);
+        }
+        if (bag[bag.size() - 1] == 's') {
+          bag.erase(bag.size() - 1, 1);
+        }
+        if (std::isdigit(bag[0])) {
+          bag.erase(0, 1);
+        }
+        if (list.find(bag) == list.end()) {
+          list[bag] = std::vector<std::string>{};
         };
+        list[bag].emplace_back(holder);
+        idx += 4;
+      };
     }
   };
-  for(auto i : nodeList){
-      std::cout << i.first << std::endl;
-  }
+
+  // BFS
+  std::unordered_set<std::string> seen;
+  std::string t = "shinygoldbag";
+  std::deque<std::string> dq{t};
+  while (dq.size() > 0) {
+    std::string x = dq[0];
+    dq.pop_front();
+    if (seen.find(x) != seen.end()) {
+      continue;
+    }
+    seen.insert(x);
+
+    for (auto y : list[x]) {
+      dq.emplace_back(y);
+    }
+  };
+  ans = seen.size() - 1;
+  return ans;
+}
+int main() {
+  std::vector<std::string> instructions;
+  loadIt(instructions);
+  std::cout << part1(instructions) << std::endl;
 };
