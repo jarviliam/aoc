e8fce815ca2c96cd3d71baecc5d05aee321d5107
jarviliam <jarviliam@gmail.com>
2021-01-15 23:44:10 +0900

    debug

--- go/day7/main.go
+++ go/day7/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/tebeka/deque"
 	"os"
 	"strings"
 )
@@ -23,8 +24,8 @@ func reader(path string) ([]string, error) {
 }
 
 type node struct {
-    id string
-    children map[string]*node
+	id       string
+	children map[string]*node
 }
 
 /*
@@ -33,43 +34,70 @@ type node struct {
  */
 func pt1(in []string) int {
 	ans := 0
-	hold := make(map[string]struct{})
+	hold := make(map[string][]string)
 	for _, v := range in {
 		if v != "" {
 			words := strings.Fields(v)
-			//holder := words[0] + words[1] + words[2]
+			holder := words[0] + words[1] + words[2]
+			holder = holder[:len(holder)-1]
+			fmt.Println(holder)
 			//If it has no bags
 			if words[len(words)-3] == "no" {
 				continue
 			} else {
 				idx := 4
 				for {
-					if idx == len(words) {
+					if idx >= len(words) {
 						break
 					}
 					bag := words[idx] + words[idx+1] + words[idx+2] + words[idx+3]
 					if bag[len(bag)-1] == '.' {
 						bag = bag[:len(bag)-1]
-						fmt.Println(bag)
 					}
 					if bag[len(bag)-1] == ',' {
 						bag = bag[:len(bag)-1]
-						fmt.Println(bag)
 					}
 					if bag[len(bag)-1] == 's' {
 						bag = bag[:len(bag)-1]
-						fmt.Println(bag)
 					}
-					if hold[bag] != struct{}{} {
-                        hold[bag] =
-						//bag = bag[:len(bag)-1]
-						//fmt.Println(bag)
+					if bag[0] >= '0' && bag[0] <= '9' {
+						fmt.Println(bag, "to ", bag[1:])
+						bag = bag[1:]
 					}
-					hold[bag] = struct{}{}
+					if _, ok := hold[bag]; !ok {
+						hold[bag] = []string{}
+					}
+					hold[bag] = append(hold[bag], holder)
+					//fmt.Println(bag)
+					idx += 4
 				}
 			}
 		}
 	}
+	for k, v := range hold {
+		fmt.Println(k, "value is ", v)
+	}
+	var exists = struct{}{}
+
+	seen := make(map[string]struct{})
+	target := "shinygoldbag"
+	q := deque.New()
+	q.append(target)
+	for {
+		if len(q) == 0 {
+			break
+		}
+		x := q.PopLeft()
+		fmt.Println(x)
+		if _, ok := seen[x]; ok {
+			continue
+		}
+		seen[x] = exists
+		for _, y := range hold[x] {
+			q.append(y)
+		}
+	}
+	fmt.Println(len(seen) - 1)
 	return ans
 }
 func pt2(in []string) int {
